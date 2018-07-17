d570e2830b2707681932c7d59406a110c5e6df63
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-17 21:56:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setActivity("En dev - Indisponible");
-    console.log("Le bot a bien été connecté");
+    console.log("NelyxBot est connecté");
 });
 
 bot.login("NDY4ODIxMTU2NDczMDc3NzYw.Di-veQ.i0fCrC6BxrN-jSVuID7l2tLjHYA");
