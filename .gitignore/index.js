b086c378da9a874e7e30dba3c9eec603e6d6af05
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-18 00:15:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,11 +10,11 @@ bot.on("ready", function() {
 bot.login("NDY4ODIxMTU2NDczMDc3NzYw.Di-veQ.i0fCrC6BxrN-jSVuID7l2tLjHYA");
 
 bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "bienvenue").send(`Bienvenue ${member}, sur le serveur de la Nelyx - Team ; avant de discuter avec les autres membres du serveur va voir les règles dans le canal {#règlement}.`)
+    member.guild.channels.find("name", "bienvenue").send(`Bienvenue ${member}, sur le serveur ${server} ; avant de discuter avec les autres membres du serveur va voir les règles dans le canal #règlement.`)
 
 })
 
 bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "bienvenue").send(`${member}, vient de quitter le serveur de la Nelyx - Team. Tant pis pour lui !`)
+    member.guild.channels.find("name", "bienvenue").send(`${member}, vient de quitter le serveur ${server}. Tant pis pour lui !`)
 
 })
