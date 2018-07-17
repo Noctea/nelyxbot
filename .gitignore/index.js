dc330cdfd538b19e6dd09a57e386f6bca120c653
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-17 23:19:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,20 +1,13 @@
 const Discord = require("discord.js");
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setActivity("En dev - Indisponible");
     console.log("NelyxBot est connecté");
 });
+
 bot.login("NDY4ODIxMTU2NDczMDc3NzYw.Di-veQ.i0fCrC6BxrN-jSVuID7l2tLjHYA");
-var prefix = ("!");
-
-bot.on('message', message => {
-
-    if (message.content === 'ping') {
-
-    message.channel.sendMessage('pong');
-
-});
 
 bot.on("guildMemberAdd", member => {
     member.guild.channels.find("name", "bienvenue").send("Bienvenue ${member}, sur le serveur de la `Nelyx - Team` ; avant de t'amuser va voir les règles dans le canal ${#règlement}.")
