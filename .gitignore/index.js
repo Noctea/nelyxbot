fbde229f0bd22699ef87adc734c580b96d9646b8
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-18 00:16:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,13 +8,3 @@ bot.on("ready", function() {
 });
 
 bot.login("NDY4ODIxMTU2NDczMDc3NzYw.Di-veQ.i0fCrC6BxrN-jSVuID7l2tLjHYA");
-
-bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "bienvenue").send(`Bienvenue ${member}, sur le serveur ${server} ; avant de discuter avec les autres membres du serveur va voir les règles dans le canal #règlement.`)
-
-})
-
-bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "bienvenue").send(`${member}, vient de quitter le serveur ${server}. Tant pis pour lui !`)
-
-})
