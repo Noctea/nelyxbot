e72f5a44753a6cac31ba0a57bbd143d5598d77d3
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-18 18:54:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setActivity("En dev - Indisponible");
+    bot.user.setActivity("En dev | !n help");
 });
 
 bot.login(process.env.TOKEN);
