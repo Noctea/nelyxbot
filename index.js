7bd4e062f94d928481bf26dd0b345ebec25b5a9c
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-18 18:50:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setActivity("En dev - Indisponible");
-    console.log("NelyxBot est connecté");
 });
 
 bot.login(process.env.TOKEN);
 
 var prefix = ("!n");
+
 
 bot.on("guildMemberAdd", member => {
     member.guild.channels.find("name", "bienvenue").send(`Bienvenue, ${member} sur le serveur de la **Nelyx - Team** ; avant de faire quoi que se soit va lire les règles qui se trouve dans le canal #règlement`)
@@ -18,3 +18,18 @@ bot.on("guildMemberAdd", member => {
 bot.on("guildMemberRemove", member => {
     member.guild.channels.find("name", "bienvenue").send(`Quel dommage, ${member} viens de quitter le serveur de la **Nelyx - Team** !`)
 })
+
+
+bot.on("message", message => {
+    if (message.content === prefix + " help"){
+        message.channel.sendMessage("Commandes : \n !n help \n !n twitter \n !n twitch");
+    }
+
+    if (message.content === prefix + " twitter"){
+        message.channel.sendMessage("[@NelyxTV] (https://twitter.com/NelyxTV)");
+    }
+
+    if (message.content === prefix + " twitch"){
+        message.channel.sendMessage("[NelyxTTV] (https://www.twitch.tv/nelyxttv) \n [NelyxFortnite] (https://www.twitch.tv/nelyxfortnite) \n [NelyxOverwatch] (https://www.twitch.tv/nelyxoverwatch)");
+    }
+});
