730b1bfad40f081b452b014df49c0497dd860479
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-19 04:41:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.on("message", message => {
 
     if (message.content === prefix + " twitter"){
         var embed = new Discord.RichEmbed()
-            .setTitle("Le Twitter")
+            .setTitle("Le Twitter :")
             .setDescription("[@NelyxTV](https://twitter.com/NelyxTV)")
             .setThumbnail("https://pbs.twimg.com/media/DiYnFwFW0AAcXQZ.jpg")
             .setColor("0x01A9DB")
@@ -45,7 +45,7 @@ bot.on("message", message => {
 
     if (message.content === prefix + " twitch"){
         var embed = new Discord.RichEmbed()
-            .setTitle("Les Chaînes Twitch")
+            .setTitle("Les Chaînes Twitch :")
             .setDescription("[NelyxTTV](https://www.twitch.tv/nelyxttv) \n [NelyxFortnite](https://www.twitch.tv/nelyxfortnite) \n [NelyxOverwatch](https://www.twitch.tv/nelyxoverwatch)")
             .setThumbnail("https://static-cdn.jtvnw.net/jtv_user_pictures/1ebfa2cc-9c03-42e7-8d41-41e0bd1f2aef-profile_image-300x300.png")
             .setColor("0x610B5E")
@@ -56,8 +56,8 @@ bot.on("message", message => {
         var embed = new Discord.RichEmbed()
             .setTitle("Candidature Nelyx - Team :")
             .setDescription("Si vous souhaitez faire une candidature pour la Nelyx - Team")
-            .addField("[Candidature Nelyx - Fortnite](https://docs.google.com/forms/d/e/1FAIpQLSd2pOi3jduntU38tSacEBHFAR26Db5dri2AIdV7-zRi75jqzw/viewform)", "")
-            .addField("[Candidature Nelyx - Overwatch](https://docs.google.com/forms/d/e/1FAIpQLSfHI9JZKm3dQs1LVxKonuKB-JxElsfS2v9U3nqtsguuq4frRg/viewform)", "")
+            .addField("Candidature Fortnite", "[Nelyx - Fortnite](https://docs.google.com/forms/d/e/1FAIpQLSd2pOi3jduntU38tSacEBHFAR26Db5dri2AIdV7-zRi75jqzw/viewform)")
+            .addField("Candidature Overwatch", "[Nelyx - Overwatch](https://docs.google.com/forms/d/e/1FAIpQLSfHI9JZKm3dQs1LVxKonuKB-JxElsfS2v9U3nqtsguuq4frRg/viewform)")
             .setThumbnail("https://pbs.twimg.com/media/DibwUXIXcAUli_m.jpg:large")
             .setColor("0x0489B1")
             .setFooter("Ce sont des formulaires à remplir obligatoirement si vous voulez intégrer la Nelyx - Team")
