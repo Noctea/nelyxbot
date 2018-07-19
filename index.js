7f599cacf5cd626a46e180b01fb14221ebe14beb
Noctea <38964329+Noctea@users.noreply.github.com>
2018-07-19 18:31:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ bot.on("message", message => {
             .addField("Nom du Discord", message.guild.name)
             .addField("Crée le", message.guild.createdAt)
             .addField("Tu as rejoint le", message.member.joinedAt)
-            .addField("Utilisateur sur le Discord n°", message.guild.memberCount)
+            .addField("Membres sur le Discord", message.guild.memberCount)
             .setThumbnail("https://pbs.twimg.com/media/DibwUXIXcAUli_m.jpg:large")
             .setColor("0x0404B4")
         message.channel.sendEmbed(embed)
